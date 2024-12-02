ebe28d36e6d915f14b1cb2c9e0a66b7ed30e22a8
DavidG256 <davgarant@alu.edu.gva.es>
2024-12-02 12:05:21 +0100

    Add files via upload

--- ArrayOperations.java
+++ ArrayOperations.java
@@ -113,13 +113,26 @@ public class ArrayOperations {
     }
 
     public static void sort (int [] array ){
-        int array2[];
+       for (int i = 0; i < array.length -1; i++){
+           for (int j = 0; j < array.length - i -1; j++){
+               if (array[j] > array[j + 1]){
+                   int temp = array[j];
+                   array[j] = array[j +1];
+                   array [j +1] = temp;
+               }
 
+           }
+       }
+
+    }
+
+    public static void copy (int []src, int[] dst ){
+        int array3[];
 
 
     }
 
-    public static void copy (){
+    void removeOddNumbers (int [] array){
 
     }
 
--- EJ2.java
+++ EJ2.java
@@ -1,6 +1,8 @@
+import java.util.Arrays;
+
 public class EJ2 {
     public static void main(String[] args) {
-        int[] array = {-2, 45, -29, 77, 56, 31};
+        int [] array = {-2, 45, -29, 77, 56, 31};
         int [] array1 = {3, 23, 32, 45};
         int [] array2 = {2, 14, 42, 18};
         ArrayOperations.print(array);
@@ -14,7 +16,14 @@ public class EJ2 {
 
         System.out.println("" + ArrayOperations.equalsWithoutOrder(array1, array2));
 
-        System.out.println("" + ArrayOperations.isArrayOn(array));
+        ArrayOperations.isArrayOn(array1,array2);
+
+       ArrayOperations.sort(array);
+        System.out.println("array ordenado"  );
+        ArrayOperations.print(array);
+
+
+
 
 
 
